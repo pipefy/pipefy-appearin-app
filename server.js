9ce90ea560ce03f1f6095bd6ae267687f0f83baf
Raphael Costa <raphael@raphaelcosta.net>
2017-06-27 18:00:01 -0300

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+// server.js
+// where your node app starts
+
+// init project
+var compression = require('compression');
+var cors = require('cors');
+var express = require('express');
+var app = express();
+
+// Enable Compression
+app.use(compression());
+
+// Enable CORS
+app.use(cors({ origin: '*' }));
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
